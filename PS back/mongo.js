bb03decd4de9b912cb4f29b1398355ce1a76ed56
JeyCraftPT <joaoduartelopi@gmail.com>
2024-05-15 12:23:31 +0100

    mudanças
    mudanças

--- PS back/mongo.js
+++ PS back/mongo.js
@@ -8,22 +8,3 @@ mongoose
   .catch(() => {
     console.log('Falhou');
   });
-
-const usersSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
-
-const collection = mongoose.model('Users', usersSchema);
-
-module.exports = collection;
